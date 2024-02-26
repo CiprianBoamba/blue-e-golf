2f8e10f9d87e47b870d235e9e0f45bd90f3cc67d
CiprianBoamba <ciprian.boamba@gmail.com>
2024-02-26 23:40:29 +0000

    handle menu-animation

--- app.js
+++ app.js
@@ -4,22 +4,32 @@ const links = menu.querySelectorAll('li');
 
 var tl = gsap.timeline({ paused: true });
 
+tl.set(menu, {
+  x: () => -menu.offsetWidth,
+});
+
+// Animate the menu to slide in from the left
 tl.to(menu, {
   duration: 1,
+  x: 0,
   opacity: 1,
-  height: '100vh',
   ease: 'expo.inOut',
 });
+
+// Animate the links
 tl.from(
   links,
   {
-    duration: 1,
+    duration: 0.5,
     opacity: 0,
-    y: 20,
-    stagger: 0.1,
+    x: -20,
+    stagger: {
+      amount: 0.5,
+      start: 0, // Ensure stagger starts from the first element even when reversing
+    },
     ease: 'expo.inOut',
   },
-  '-=0.5',
+  '-=0.75',
 );
 
 tl.reverse();
@@ -30,16 +40,4 @@ ham.addEventListener('click', () => {
 
 document.getElementById('menu-toggle').addEventListener('click', function () {
   document.body.classList.toggle('nav-open');
-});
-
-const nav = document.querySelector('nav');
-
-ham.addEventListener('click', () => {
-  nav.classList.toggle('nav-open');
-});
-
-const menuToggle = document.querySelector('#menu-toggle');
-
-menuToggle.addEventListener('click', function () {
-  nav.classList.toggle('nav-open');
 });
