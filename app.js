aa6d5cf4e7b0b8b13742df7ec8b5344b78a1da6c
CiprianBoamba <ciprian.boamba@gmail.com>
2024-03-03 13:40:01 +0000

    add more

--- app.js
+++ app.js
@@ -1,64 +1,230 @@
 /**  -- Hamburger Menu Functionality -- */
 
-const ham = document.querySelector('.ham');
-const menu = document.querySelector('ul.main-menu');
-const links = menu.querySelectorAll('li');
+const menuToggleOpen = document.querySelector('#menu-toggle-open');
+const menuToggleClose = document.querySelector('#menu-toggle-close');
+const menu = document.querySelector('.menu');
 
-var tl = gsap.timeline({ paused: true });
-
-tl.set(menu, {
-  x: () => -menu.offsetWidth,
+const tl = gsap.timeline({
+  paused: true,
 });
 
-// Animate the menu to slide in from the left
-tl.to(menu, {
-  duration: 1,
+tl.to('.menu', {
   x: 0,
-  opacity: 1,
+  duration: 0.6,
   ease: 'expo.inOut',
 });
 
-// Animate the links
-tl.from(
-  links,
-  {
-    duration: 0.5,
-    opacity: 0,
-    x: -20,
-    stagger: {
-      amount: 0.5,
-      start: 0,
+tl.from('.menu nav a', {
+  y: 40,
+  opacity: 0,
+  duration: 0.4,
+  ease: 'expo.out',
+  stagger: 0.1,
+});
+
+tl.from('.social', {
+  y: 40,
+  opacity: 0,
+  duration: 0.4,
+  ease: 'expo.out',
+});
+
+function quickReverse() {
+  const reverseTl = gsap.timeline({
+    onComplete: () => tl.pause(0),
+  });
+
+  reverseTl.to(
+    '.social',
+    {
+      y: 40,
+      opacity: 0,
+      duration: 0.2,
+      ease: 'expo.in',
     },
-    ease: 'expo.inOut',
-  },
-  '-=0.75',
-);
+    0,
+  );
 
-tl.reverse();
+  reverseTl.to(
+    '.menu nav a',
+    {
+      y: 40,
+      opacity: 0,
+      duration: 0.5,
+      ease: 'expo.in',
+      stagger: 0.05,
+    },
+    0,
+  );
 
-ham.addEventListener('click', () => {
-  tl.reversed(!tl.reversed());
+  reverseTl.to(
+    '.menu',
+    {
+      x: '-100%',
+      duration: 0.5,
+      ease: 'expo.in',
+    },
+    0,
+  );
+}
+
+menuToggleOpen.addEventListener('click', function () {
+  tl.restart();
+
+  gsap.to(menuToggleOpen, {
+    opacity: 0,
+    duration: 0.3,
+    onComplete: () => {
+      menuToggleOpen.style.visibility = 'hidden';
+      menuToggleClose.style.visibility = 'visible';
+      menuToggleClose.style.opacity = 1;
+    },
+  });
 });
 
-document.getElementById('menu-toggle').addEventListener('click', function () {
-  document.body.classList.toggle('nav-open');
+menuToggleClose.addEventListener('click', function () {
+  quickReverse();
+
+  gsap.to(menuToggleClose, {
+    opacity: 0,
+    duration: 0.3,
+    onComplete: () => {
+      menuToggleClose.style.visibility = 'hidden';
+      menuToggleOpen.style.visibility = 'visible';
+      gsap.to(menuToggleOpen, {
+        opacity: 1,
+        duration: 0.3,
+      });
+    },
+  });
 });
 
-/**  -- Search  Functionality -- */
+menuToggleOpen.addEventListener('click', () => tl.play());
+menuToggleClose.addEventListener('click', quickReverse);
 
-document.addEventListener('DOMContentLoaded', () => {
-  const searchIcon = document.querySelector('.search-icon');
-  const closeIcon = document.querySelector('.close');
+/**  -- Search Button Functionality -- */
+const searchClose = document.querySelector('.search-close');
+const searchBtn = document.querySelector('.search-btn');
+const searchOverlay = document.getElementById('search-overlay');
+const searchButton = document.getElementById('search-button');
+const searchText = document.getElementById('search-text');
+const searchQueryDisplay = document.querySelector('.search-query-display');
 
-  const searchToggle = (obj) => {
-    const container = obj.closest('.search-wrapper');
-    container.classList.toggle('active');
+searchClose.addEventListener('click', () => {
+  searchOverlay.style.display = 'none';
+  searchBtn.style.display = 'block';
+  searchQueryDisplay.textContent = '';
+});
 
-    if (!container.classList.contains('active')) {
-      container.querySelector('.search-input').value = '';
+searchBtn.addEventListener('click', () => {
+  searchBtn.style.display = 'none';
+  searchOverlay.style.display = 'block';
+});
+searchButton.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const searchValue = searchText.value;
+  searchQueryDisplay.innerHTML = `Search for : <i>${searchValue}</i>`;
+  searchText.value = '';
+});
+
+/**  -- Top Slider Functionality -- */
+
+$('.slider').each(function () {
+  let $this = $(this);
+  let $group = $this.find('.slide_group');
+  let $slides = $this.find('.slide');
+  let bulletArray = [];
+  let currentIndex = 0;
+  let timeout;
+
+  function move(newIndex) {
+    let animateLeft, slideLeft;
+
+    if ($group.is(':animated') || currentIndex === newIndex) {
+      return;
     }
-  };
 
-  searchIcon.addEventListener('click', () => searchToggle(searchIcon));
-  closeIcon.addEventListener('click', () => searchToggle(closeIcon));
+    bulletArray[currentIndex].removeClass('active');
+    bulletArray[newIndex].addClass('active');
+
+    if (newIndex > currentIndex) {
+      slideLeft = '100%';
+      animateLeft = '-100%';
+    } else {
+      slideLeft = '-100%';
+      animateLeft = '100%';
+    }
+
+    $slides.eq(newIndex).css({
+      display: 'block',
+      left: slideLeft,
+    });
+    $group.animate(
+      {
+        left: animateLeft,
+      },
+      function () {
+        $slides.eq(currentIndex).css({
+          display: 'none',
+        });
+        $slides.eq(newIndex).css({
+          left: 0,
+        });
+        $group.css({
+          left: 0,
+        });
+        currentIndex = newIndex;
+      },
+    );
+  }
+
+  $('.next_btn').on('click', function () {
+    if (currentIndex < $slides.length - 1) {
+      move(currentIndex + 1);
+    } else {
+      move(0);
+    }
+  });
+
+  $('.previous_btn').on('click', function () {
+    if (currentIndex !== 0) {
+      move(currentIndex - 1);
+    } else {
+      move($slides.length - 1);
+    }
+  });
+
+  $.each($slides, function (index) {
+    let $button = $('<a class="slide_btn">&bull;</a>');
+
+    if (index === currentIndex) {
+      $button.addClass('active');
+    }
+    $button
+      .on('click', function () {
+        move(index);
+      })
+      .appendTo('.slide_buttons');
+    bulletArray.push($button);
+  });
+});
+
+// Listen to the scroll event
+
+gsap.registerPlugin(ScrollTrigger);
+
+const textElements = gsap.utils.toArray('.text');
+
+textElements.forEach((text) => {
+  gsap.to(text, {
+    backgroundSize: '100%',
+    ease: 'none',
+    scrollTrigger: {
+      trigger: text,
+      start: 'center 80%',
+      end: 'center 20%',
+      scrub: true,
+    },
+  });
 });
